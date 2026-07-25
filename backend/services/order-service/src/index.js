1e8241518d5d27e6f5dc1b709bdadde26190f71b
pawan <apavanreddy1003@gmail.com>
2026-07-25 22:12:54 +0530

    feat: scaffold initial order-service with CRUD operations and database configuration

--- backend/services/order-service/src/index.js
+++ backend/services/order-service/src/index.js
@@ -1,3 +1,10 @@
 const app = require('./app');
-const { port } = require('./config/config');
-app.listen(port, () => console.log(`order-service listening on ${port}`));
+const config = require("./config/config");
+const connectDB = require("./config/db");
+connectDB().then(()=>{
+    console.log("Database connected successfully");
+    app.listen(config.port, () => console.log(`order-service listening on port: http://localhost:${config.port}`));
+}).catch((err)=>{
+    console.error("Database connection error:", err);
+    process.exit(1);
+});
--- backend/services/order-service/src/schema/order.schema.js
+++ backend/services/order-service/src/schema/order.schema.js
@@ -1,2 +1,47 @@
-function validateOrder(input) { return input && input.userId && Array.isArray(input.items) && input.items.length ? null : 'userId and at least one item are required'; }
-module.exports = { validateOrder };
+const mongoose = require("mongoose");
+const orderSchema = new mongoose.Schema({
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user",
+        required:true
+    },
+    items:[{
+            productId:{
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"product",
+                required:true
+            },
+            quantity:{
+                type:Number,
+                required:true,
+                default:1
+            },
+            price:{
+                type:Number,
+                required:true,
+                default:0
+            }
+        }
+    ],
+    totalAmount:{
+        type:Number,
+        required:true,
+        default:0
+    },
+    shippingAddress:{
+        type:String,
+        required:true
+    },
+    status:{
+        type:String,
+        required:true,
+        default:"pending"
+    },
+    paymentStatus:{
+        type:String,
+        required:true,
+        default:"pending"
+    }
+},{timestamps:true})
+
+module.exports =  orderSchema;
