82e0f3d6efb6f77c9356bf6ded82905e5d94ca72
pawan <apavanreddy1003@gmail.com>
2026-07-25 22:41:31 +0530

    payment services curd operations

--- backend/services/payment-service/src/index.js
+++ backend/services/payment-service/src/index.js
@@ -1,3 +1,11 @@
 const app = require('./app');
-const { port } = require('./config/config');
-app.listen(port, () => console.log(`payment-service listening on ${port}`));
+const config = require('./config/config');
+const connectDB = require('./config/db');
+
+connectDB().then(() => {
+    console.log("Database connected successfully");
+    app.listen(config.port, () => console.log(`payment-service listening on port: http://localhost:${config.port}`));
+}).catch((err) => {
+    console.error("Database connection error:", err);
+    process.exit(1);
+});
