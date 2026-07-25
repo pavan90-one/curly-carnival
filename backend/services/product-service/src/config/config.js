eee4ad56c7e0afad1d074077b1d0b99280bbdc0c
pawan <apavanreddy1003@gmail.com>
2026-07-25 17:14:39 +0530

    feat: initialize product-service backend microservice and add CI/CD pipeline workflows

--- backend/services/product-service/src/config/config.js
+++ backend/services/product-service/src/config/config.js
@@ -1,1 +1,7 @@
-module.exports = { port: Number(process.env.PORT) || 4002 };
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").env;
+const config = {
+       port: Number(process.env.PORT) || 4002,
+       data_Url :process.env.DATABASE_URL || "mongodb://127.0.0.1:27017/smart-commerce-products"
+}
+module.exports = config;
